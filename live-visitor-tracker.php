199c4059c0d623a82a6ea22a5ed88838e33a8db1
Roy Sivan <roy.x.sivan.-ND@disney.com>
2015-08-07 01:04:19 -0700

    initial commit

---
+++ live-visitor-tracker.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Live Visitor Tracker
+ * Plugin URI: https://github.com/royboy789/Live-Visitor-Tracker
+ * Description: Live Visitor tracker allows you to see who is on your website and what page they are on. LIVE.
+ * Version: 1.0
+ * Author: Roy Sivan
+ * Text Domain: live_visitor_tracker
+ * Author URI: http://www.roysivan.com
+ * License: GPL2
+ * GitHub Plugin URI: https://github.com/royboy789/wp-api-social-login
+ * GitHub Branch: master
+*/
+
+
+
+
+
+?>
